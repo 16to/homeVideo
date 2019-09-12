1bd454e4c0668d10350eb0e3b45fadad873b34ad
16to <zj@16to.com>
2019-09-12 22:37:34 +0800

    list done

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 //引入express中间件
 const express = require('express');
+const bodyParser = require('body-parser');
 const path = require('path');
 const app = express();
 
 app.use(express.static(path.join(__dirname,'/')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get("/api/list",(req,res)=>{
+    console.log(123);
+    const arr = [{"src":"/uc/1.mp4"},{"src":"/uc/2.mp4"}]; 
+    res.send(JSON.stringify(arr));
+});
 
 //监听端口为8080
 const server = app.listen(8080, function () {
